4a57481bf418cad51d81a00e906e70b2100167b4
xAlexCode <alexandra.henriksson00@gmail.com>
2026-03-18 15:30:38 +0100

    Installed npm and solved errors to get the page running, like import toggleLightMode and remove i++ from the API loop  update  #1

--- src/js/createHtml.js
+++ src/js/createHtml.js
@@ -8,10 +8,10 @@ let i = 0;
 export async function createHtml (){
     const podCasts = await getPodcasts ();
     podCasts.programs.forEach((podcast) => {
-    i++
+   
     const innerArticle = createInnerArticle();
 
-createImg();
+    createImg();
 
     const textDiv = createTextDiv();
 
@@ -27,7 +27,7 @@ createImg();
             return innerArticle;
     }
 
-    function createTextiv() {
+    function createTextDiv() {
         const textDiv = document.createElement('div');
         textDiv.setAttribute('class', 'section__article-div');
         innerArticle.appendChild(textDiv);
@@ -37,14 +37,14 @@ createImg();
     function createLink (){
         const linkPlacement = document.createElement('a');
         const linkText = document.createTextNode('Lyssna här');
-        linkPlacement.setAttribute('href', podCasts.programs[i].programurl);
+        linkPlacement.setAttribute('href', podcast.programurl);
         linkPlacement.setAttribute('tabindex', '1')
         linkPlacement.appendChild(linkText);
         textDiv.appendChild(linkPlacement)
     }
     function createImg() {
         const imgPlacement = document.createElement('IMG');
-        imgPlacement.setAttribute('src', podCasts.programs[i].socialimage);
+        imgPlacement.setAttribute('src', podcast.socialimage);
         imgPlacement.setAttribute('width', '100');
         imgPlacement.setAttribute('height', '100')
         innerArticle.appendChild(imgPlacement);
@@ -52,14 +52,14 @@ createImg();
 
     function createP() {
     const descPlacement = document.createElement('p');
-    const desc = document.createTextNode(podCasts.programs[i].description);
+    const desc = document.createTextNode(podcast.description);
     descPlacement.appendChild(desc);
     textDiv.appendChild(descPlacement);
 }
 
 function createHeader() {
     const headerPlacement = document.createElement('h2');
-    const programName = document.createTextNode(podCasts.programs[i].name);
+    const programName = document.createTextNode(podcast.name);
     headerPlacement.appendChild(programName);
     textDiv.appendChild(headerPlacement);
 }
