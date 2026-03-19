21618dbe3d86a5cbdbde32d607853c6acc59b7a3
xAlexCode <alexandra.henriksson00@gmail.com>
2026-03-19 15:36:54 +0100

    Installed biome, configured rules. Did lint and format and solved issues that came with it close #5

--- src/ts/api.ts
+++ src/ts/api.ts
@@ -1,20 +1,22 @@
 let podcasts = {};
 
 if (import.meta.env.DEV) {
-    const module = await import('./data/podcasts.json'); // Laddar in JSON filen dynamiskt i Dev mode
-    podcasts = module.default;
+  const module = await import("./data/podcasts.json"); // Laddar in JSON filen dynamiskt i Dev mode
+  podcasts = module.default;
 } else {
-    podcasts = await fetch('https://api.sr.se/api/v2/programs/index?programcategoryid=133&format=json&pagination=false&indent=true&filter=program.archived&filterValue=false') // Använder riktiga API när npm run build körts
-        .then((data) => data.json())
-        .then((json) => json)
-        .catch((error) => {
-            console.error('nått blev fel:', error)
-            return null;
-        })
+  podcasts = await fetch(
+    "https://api.sr.se/api/v2/programs/index?programcategoryid=133&format=json&pagination=false&indent=true&filter=program.archived&filterValue=false",
+  ) // Använder riktiga API när npm run build körts
+    .then((data) => data.json())
+    .then((json) => json)
+    .catch((error) => {
+      console.error("nått blev fel:", error);
+      return null;
+    });
 }
 
 export async function getPodcasts() {
-    return podcasts;
+  return podcasts;
 }
 
 export default getPodcasts;
--- src/ts/createHtml.ts
+++ src/ts/createHtml.ts
@@ -1,14 +1,16 @@
-import type { IPodcast, IPodcastResponse } from './types.ts'; // Importera interfacen 
-import {getPodcasts} from './api.ts'; 
+import { getPodcasts } from "./api.ts";
+import type { IPodcast, IPodcastResponse } from "./types.ts"; // Importera interfacen
 
-
-const podCastContainer = document.querySelector('.podListContainer') as HTMLElement; // Säga att det är ett html-element annars klagar den på null
+const podCastContainer = document.querySelector(
+  ".podListContainer",
+) as HTMLElement; // Säga att det är ett html-element annars klagar den på null
 
 //let i = 0; kanske behöver senare
 
-export async function createHtml (){
-    const podCasts = await getPodcasts () as IPodcastResponse; // Lova ts att datan följer interfacet
-    podCasts.programs.forEach((podcast: IPodcast) => { //Lägg till index igen senare
+export async function createHtml() {
+  const podCasts = (await getPodcasts()) as IPodcastResponse; // Lova ts att datan följer interfacet
+  podCasts.programs.forEach((podcast: IPodcast) => {
+    //Lägg till index igen senare
     console.log("API-svar:", podCasts); // Logga API svaret, dock hela listan
     const innerArticle = createInnerArticle();
 
@@ -21,51 +23,50 @@ export async function createHtml (){
     createLink();
 
     function createInnerArticle() {
-        const innerArticle = document.createElement('article');
-        innerArticle.classList.add('podListSections');
-            innerArticle.setAttribute('tabindex', '1')
-            podCastContainer.appendChild(innerArticle);
-            return innerArticle;
+      const innerArticle = document.createElement("article");
+      innerArticle.classList.add("podListSections");
+      innerArticle.setAttribute("tabindex", "1");
+      podCastContainer.appendChild(innerArticle);
+      return innerArticle;
     }
 
     function createTextDiv() {
-        const textDiv = document.createElement('div');
-        textDiv.classList.add('podListContent');
-        innerArticle.appendChild(textDiv);
-        return textDiv;
+      const textDiv = document.createElement("div");
+      textDiv.classList.add("podListContent");
+      innerArticle.appendChild(textDiv);
+      return textDiv;
     }
 
-    function createLink (){
-        const linkPlacement = document.createElement('a');
-        const linkText = document.createTextNode('Lyssna här');
-        linkPlacement.setAttribute('href', podcast.programurl); // inuti loopen behöver den inte index (program[i]) då den har tillgång till APIn
-        linkPlacement.setAttribute('tabindex', '1')
-        linkPlacement.appendChild(linkText);
-        textDiv.appendChild(linkPlacement)
+    function createLink() {
+      const linkPlacement = document.createElement("a");
+      const linkText = document.createTextNode("Lyssna här");
+      linkPlacement.setAttribute("href", podcast.programurl); // inuti loopen behöver den inte index (program[i]) då den har tillgång till APIn
+      linkPlacement.setAttribute("tabindex", "1");
+      linkPlacement.appendChild(linkText);
+      textDiv.appendChild(linkPlacement);
     }
     function createImg() {
-        const imgPlacement = document.createElement('IMG');
-        imgPlacement.setAttribute('src', podcast.socialimage);
-        imgPlacement.setAttribute('width', '100');
-        imgPlacement.setAttribute('height', '100')
-        innerArticle.appendChild(imgPlacement);
+      const imgPlacement = document.createElement("IMG");
+      imgPlacement.setAttribute("src", podcast.socialimage);
+      imgPlacement.setAttribute("width", "100");
+      imgPlacement.setAttribute("height", "100");
+      innerArticle.appendChild(imgPlacement);
     }
 
     function createP() {
-    const descPlacement = document.createElement('p');
-    const desc = document.createTextNode(podcast.description);
-    descPlacement.appendChild(desc);
-    textDiv.appendChild(descPlacement);
-}
+      const descPlacement = document.createElement("p");
+      const desc = document.createTextNode(podcast.description);
+      descPlacement.appendChild(desc);
+      textDiv.appendChild(descPlacement);
+    }
 
-function createHeader() {
-    const headerPlacement = document.createElement('h2');
-    const programName = document.createTextNode(podcast.name);
-    headerPlacement.appendChild(programName);
-    textDiv.appendChild(headerPlacement);
-}
-    
-})
+    function createHeader() {
+      const headerPlacement = document.createElement("h2");
+      const programName = document.createTextNode(podcast.name);
+      headerPlacement.appendChild(programName);
+      textDiv.appendChild(headerPlacement);
+    }
+  });
 }
 
 export default createHtml;
